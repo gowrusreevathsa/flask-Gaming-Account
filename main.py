c401f11419412144148688d6c61d71ad2ac9cf01
Sreevathsa <sreevathsagowru@gmail.com>
2020-06-06 16:03:20 +0530

    index and sign up page basic creation

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/signUp/')
+def signUp():
+    return render_template('signUp.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
