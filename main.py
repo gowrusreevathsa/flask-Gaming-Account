9ef7406fda0da4150c62faa4c1758faa9b76bc0f
Sreevathsa <sreevathsagowru@gmail.com>
2020-06-08 13:12:49 +0530

    Testing sockets

--- main.py
+++ main.py
@@ -67,6 +67,10 @@ def home():
 def  storeGameData():
     pass
 
+@socketio.on('testEvent')
+def handle_testEvent(msg):
+    print(msg)
+
 if __name__ == '__main__':
     client = MongoClient("mongodb://127.0.0.1:27017")
     db = client['GamingAccount']
